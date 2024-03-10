d03e0b5b74916a03866566c65c076a7d30b459fa
MasoudSh <Msh.Git.Dev@gmail.com>
2024-03-10 12:21:09 +0330

    create : EntityId

---
+++ Domains/Domains.Auth/ValueObjects/EntityId.cs
@@ -0,0 +1,19 @@
+﻿using Shared.Auth.Exceptions;
+
+namespace Domains.Auth.ValueObjects;
+internal class EntityId {
+    public Guid Value { get; }
+
+    private EntityId() => Value = Guid.NewGuid();
+    public EntityId(Guid entityId) {
+        if(entityId == Guid.Empty) {
+            throw new InvalidGuidException();
+        }
+        Value = entityId;
+    }
+
+    public static EntityId Create() => new();
+    public static implicit operator Guid(EntityId entityId) => entityId.Value;
+    public static implicit operator EntityId(Guid guid) => new(guid);
+
+}
