785b235024813329acccf5b2fa4905c531d66e84
MasoudSh <Msh.Git.Dev@gmail.com>
2024-03-10 12:21:35 +0330

    create : CustomException,InvalidGuidException

---
+++ Shared.Auth/Exceptions/CustomException.cs
@@ -0,0 +1,22 @@
+﻿namespace Shared.Auth.Exceptions;
+public abstract class CustomException :Exception {
+
+    public string Code { get; private set; } = "<not-defined>";
+    public string Description { get; private set; } = "<not-defined>";
+
+    public CustomException() : base() { }
+
+    public CustomException(string code , string description) :base(description)
+    {
+        Code = code; 
+        Description = description;
+    }
+    public CustomException(string description) : base(description) {
+        Description = description;
+    }
+
+    public void Update(string code) {
+        this.Code = code;
+    }
+
+}
---
+++ Shared.Auth/Exceptions/InvalidGuidException.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Auth.Exceptions;
+public class InvalidGuidException : CustomException {
+    public InvalidGuidException(string description = "The Guid value can not be all zeros.") : base(description) {
+        this.Update("Invalid-Guid");
+    }
+
+    public InvalidGuidException(string code , string description) : base(code , description) {
+    }
+}
