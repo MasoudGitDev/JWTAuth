6278c8b31692f144b60bfa9b4f6a1014820599d3
MasoudSh <Msh.Git.Dev@gmail.com>
2024-03-11 10:02:43 +0330

    create : ChangeEvent to capture RaiseEvent

---
+++ Domains/Domains.Auth/Shared/Events/ChangeEvent.cs
@@ -0,0 +1,14 @@
+﻿using Domains.Auth.Shared.Abstractions;
+using Domains.Auth.Shared.Enums;
+
+namespace Domains.Auth.Shared.Events;
+
+internal class ChangeEvent<T>(string propertyName, T from, T to) : IDomainEvent
+{
+    public EventType EventType => EventType.Modified;
+
+    public string? Description
+        => $"The <{propertyName}> is changed [from : <{from}>] => [to : <{to}>].";
+}
+
+
