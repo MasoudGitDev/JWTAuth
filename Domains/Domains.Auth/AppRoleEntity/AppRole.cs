91ae95bd7444b7e8ebbc2d9634f21f6b2485e37e
MasoudSh <Msh.Git.Dev@gmail.com>
2024-03-11 10:05:48 +0330

    Create : AppRole Entity

---
+++ Domains/Domains.Auth/AppRoleEntity/AppRole.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domains.Auth.AppRoleEntity;
+internal class AppRole : IdentityRole<Guid> {
+
+    //public DateTime CreatedAt { get; private set; }
+    //public AppUserId CreatorId { get; set; }
+    //public AppUser CreatedOrModifiedBy { get; set; }
+
+}
