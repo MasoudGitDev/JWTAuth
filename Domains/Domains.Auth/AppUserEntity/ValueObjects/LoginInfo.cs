bc553239bbad4e15686fe026b3f7e1067473fc92
MasoudSh <Msh.Git.Dev@gmail.com>
2024-03-12 23:40:24 +0330

    create : LoginInfo

---
+++ Domains/Domains.Auth/AppUserEntity/ValueObjects/LoginInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace Domains.Auth.AppUserEntity.ValueObjects;
+internal class LoginInfo {
+    public string MacAddress { get; private set; } = "<undefined>";
+    public string BrowserName { get; private set; } = "<undefined>";
+    public string? UniqueId { get; set; }
+    public bool CanAccess { get; private set; } = true;
+}
