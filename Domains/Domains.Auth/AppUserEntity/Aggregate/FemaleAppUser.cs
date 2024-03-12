0908caeb3bdf770785f2a31d61cabc3ff073c067
MasoudSh <Msh.Git.Dev@gmail.com>
2024-03-12 23:37:55 +0330

    update : use Table attribute

--- Domains/Domains.Auth/AppUserEntity/Aggregate/FemaleAppUser.cs
+++ Domains/Domains.Auth/AppUserEntity/Aggregate/FemaleAppUser.cs
@@ -1,3 +1,6 @@
-﻿namespace Domains.Auth.AppUserEntity.Aggregate;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
+namespace Domains.Auth.AppUserEntity.Aggregate;
+
+[Table("FemaleAppUsers")]
 internal class FemaleAppUser : AppUser { }
