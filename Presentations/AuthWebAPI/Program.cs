34bbb5a22e7657b72911a876025ffa44ce64560f
MasoudSh <Msh.Git.Dev@gmail.com>
2024-03-12 23:41:35 +0330

    update : add some configs for  mediatR , Authentication , jwtBearer and Swagger

--- Presentations/AuthWebAPI/Program.cs
+++ Presentations/AuthWebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Infra.Auth.ServiceConfigs;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,44 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMediatR(opt => {
+    opt.RegisterServicesFromAssemblies(
+        typeof(Domains.Auth.Shared.Abstractions.IDomainEvent).Assembly
+    );
+});
+
+builder.Services.AddAuthentication(opt => {
+    string bearer = "Bearer";
+    opt.DefaultScheme = bearer;
+    opt.DefaultChallengeScheme = bearer;
+    opt.DefaultAuthenticateScheme = bearer;    
+}).AddJwtBearer();
+
+builder.Services.AddSwaggerGen(opt=> {
+    opt.SwaggerDoc("v1" , new OpenApiInfo() { Title = "Jwt Authentication" });
+    var openApiSecurityScheme = new OpenApiSecurityScheme(){
+        Scheme = "Bearer" ,
+        BearerFormat = "JWT" ,
+        Description = "Put your jwt token in this textbox below !" ,
+        In = ParameterLocation.Header ,
+        Name = "Jwt Authentication" ,
+        Type = SecuritySchemeType.Http ,
+        Reference = new OpenApiReference{
+            Id = "Bearer" ,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement() {
+        
+        {openApiSecurityScheme , Array.Empty<string>() }
+    });
+    opt.AddSecurityDefinition("Bearer" , openApiSecurityScheme);
+
+});
+
+builder.Services.Add_InfraAuth_Services();
+
 
 var app = builder.Build();
 
@@ -17,6 +58,7 @@ if(app.Environment.IsDevelopment()) {
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
