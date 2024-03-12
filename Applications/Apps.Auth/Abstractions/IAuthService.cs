0c3bd11abdd95ffd398cf870fe627488fa2721f0
MasoudSh <Msh.Git.Dev@gmail.com>
2024-03-12 16:28:01 +0330

    create : IAuthService and JwtService

---
+++ Applications/Apps.Auth/Abstractions/IAuthService.cs
@@ -0,0 +1,8 @@
+﻿using Shared.Auth.Models;
+using Shared.Auth.ValueObjects;
+
+namespace Apps.Auth.Abstractions;
+public interface IAuthService {
+    Task<AccountResult> GenerateTokenAsync(AppUserId appUserId);
+    Task<AccountResult> EvaluateAsync(string token);
+}
---
+++ Applications/Apps.Auth/Services/JwtService.cs
@@ -0,0 +1,45 @@
+﻿using Apps.Auth.Abstractions;
+using Jose;
+using Shared.Auth.Enums;
+using Shared.Auth.Extensions;
+using Shared.Auth.Models;
+using Shared.Auth.ValueObjects;
+using System.Text;
+
+namespace Apps.Auth.Services;
+
+internal class JwtService(AuthTokenSettings tokenSettings) : IAuthService {
+
+    private readonly JwsAlgorithm _algorithm  = JwsAlgorithm.HS256;
+    private readonly byte[] _securityKey = Encoding.UTF8.GetBytes(tokenSettings.secureKey);
+
+    public async Task<AccountResult> GenerateTokenAsync(AppUserId appUserId) {
+        var claims = GenerateClaims(appUserId);
+        return ( new AccountResult(ResultStatus.Succeed , await EncodeAsync(claims) , claims) );
+    }
+
+    private Dictionary<string , string> GenerateClaims(AppUserId appUserId)
+        => new() {
+            { AuthTokenType.Id , Guid.NewGuid().ToString() },
+            { AuthTokenType.UserId , appUserId } ,
+            { AuthTokenType.IssuerAt , DateTime.UtcNow.ToString() } ,
+            { AuthTokenType.Issuer , tokenSettings.Issuer } ,
+            { AuthTokenType.Audience , tokenSettings.Audience } ,
+            { AuthTokenType.Expire , DateTime.UtcNow.AddMinutes(tokenSettings.ExpireMinutes).ToString() } ,
+        };
+
+    public async Task<AccountResult> EvaluateAsync(string token)
+        => new AccountResult(ResultStatus.Succeed , token , await DecodeAsync(token));
+
+    private Task<string> EncodeAsync(Dictionary<string , string> claims)
+        => Task.FromResult(( JWT.Encode(claims , _securityKey , _algorithm) )
+            .ThrowIfNullOrWhiteSpace("System can not generate an <auth-token>."));
+
+    private Task<Dictionary<string , string>> DecodeAsync(string token) {
+        return Task.FromResult(( JWT.Decode(token , _securityKey , _algorithm) )
+            .ThrowIfNullOrWhiteSpace("<Auth-Token>")
+            .FromJsonToType<Dictionary<string , string>>()
+            .ThrowIfNull("<claims>")
+            .ThrowIfEmpty("<claims>"));
+    }
+}
