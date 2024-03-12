1d23153c7339474f256374f5967a71b53bdacfca
MasoudSh <Msh.Git.Dev@gmail.com>
2024-03-12 19:28:17 +0330

    create : AppUserId ValueObject

---
+++ Shared.Auth/ValueObjects/AppUserId.cs
@@ -0,0 +1,23 @@
+﻿using Shared.Auth.Exceptions;
+
+namespace Shared.Auth.ValueObjects;
+public class AppUserId :IEquatable<Guid> {
+    public Guid Value { get; }
+
+    private AppUserId() => Value = Guid.NewGuid();
+    public AppUserId(Guid entityId) {
+        if(entityId == Guid.Empty) {
+            throw new InvalidGuidException();
+        }
+        Value = entityId;
+    }
+
+    public static AppUserId Create() => new();
+
+    public bool Equals(Guid other) => Value == other;
+
+    public static implicit operator Guid(AppUserId entityId) => entityId.Value;
+    public static implicit operator AppUserId(Guid guid) => new(guid);
+    public static implicit operator string(AppUserId entityId) => entityId.Value.ToString();
+
+}
