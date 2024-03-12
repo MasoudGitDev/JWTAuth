fccb23158f04c7ffe617193c71f85cfdce3aacd9
MasoudSh <Msh.Git.Dev@gmail.com>
2024-03-12 19:31:45 +0330

    create : DTOs (SignUp)

---
+++ Presentations/AuthWebAPI/DTOs/SignUpDto.cs
@@ -0,0 +1,20 @@
+﻿using Shared.Auth.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthWebAPI.DTOs;
+
+public class SignUpDto {
+    required public string Email { get; set; }
+    required public string UserName { get; set; }
+    required public DateTime BirthDate { get; set; } = DateTime.UtcNow;
+    required public Gender Gender { get; set; } = Gender.Male;
+
+    required public string Password { get; set; }
+
+    [Compare(nameof(Password))]
+    required public string ConfirmedPassword { get; set; }
+
+    public string? Captcha { get; set; }
+
+
+}
