a8d9499a0964ad69fafa8281c9c4e0142fb38161
MasoudSh <Msh.Git.Dev@gmail.com>
2024-03-12 19:30:15 +0330

    create : Exceptions( EmptyList,NotNull)

---
+++ Shared.Auth/Exceptions/EmptyListException.cs
@@ -0,0 +1,6 @@
+﻿namespace Shared.Auth.Exceptions;
+public class EmptyListException : CustomException {
+    public EmptyListException(string description) : base(description) {
+        Update("Empty");
+    }
+}
---
+++ Shared.Auth/Exceptions/NotNullException.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Auth.Exceptions;
+public class NotNullException : CustomException {
+    public NotNullException(string description) : base(description) {
+    }
+
+    public NotNullException(string code , string description) : base(code , description) {
+    }
+}
