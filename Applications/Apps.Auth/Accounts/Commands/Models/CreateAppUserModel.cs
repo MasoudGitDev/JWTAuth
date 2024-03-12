314cf31529d348b5703aa5bacabed109d8e5e61f
MasoudSh <Msh.Git.Dev@gmail.com>
2024-03-12 19:39:00 +0330

    create : [ CreateMaleUserHandler and its model ] , [Accountmanager and its queries]

---
+++ Applications/Apps.Auth/Accounts/Commands/Models/CreateAppUserModel.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Shared.Auth.Enums;
+using Shared.Auth.Models;
+
+namespace Apps.Auth.Accounts.Commands.Models;
+public class CreateAppUserModel :IRequest<AccountResult> {
+
+    required public string Email { get; set; }
+    required public string UserName { get; set; }
+    required public string Password { get; set; }
+
+    required public DateTime BirthDate { get; set; } = DateTime.UtcNow;
+    required public Gender Gender { get; set; } = Gender.Male;
+}
---
+++ Applications/Apps.Auth/Managers/AccountManager_Queries.cs
@@ -0,0 +1,12 @@
+﻿using Domains.Auth.AppUserEntity.Aggregate;
+
+namespace Apps.Auth.Managers;
+internal abstract partial class AccountManager<TModel, TReturn> {
+
+    protected async Task<AppUser?> FindByEmailAsync(string email)
+        => await _userManager.FindByEmailAsync(email);
+
+    protected async Task<AppUser?> FindByUserName(string userName)
+        => await _userManager.FindByNameAsync(userName);
+
+}
