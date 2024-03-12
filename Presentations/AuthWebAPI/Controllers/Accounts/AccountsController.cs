b53c8d33b8aeef9c0118e4d5c980e79c8077cad2
MasoudSh <Msh.Git.Dev@gmail.com>
2024-03-12 19:32:40 +0330

    create : api Controller ( Accounts)

---
+++ Presentations/AuthWebAPI/Controllers/Accounts/AccountsController.cs
@@ -0,0 +1,28 @@
+﻿using Apps.Auth.Accounts.Commands.Models;
+using AuthWebAPI.DTOs;
+using Mapster;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Auth.Models;
+
+namespace AuthWebAPI.Controllers.Accounts;
+[Route("api/[controller]")]
+[ApiController]
+public class AccountsController(ISender sender) : ControllerBase {
+
+    [HttpGet("SignUp")]
+    public async Task<AccountResult> SignUp([FromForm] SignUpDto model) {     
+        return await sender.Send(model.Adapt<CreateAppUserModel>());
+    }
+
+    [HttpGet("SignIn")]
+    public async Task SignIn() {
+
+    }
+
+    [HttpGet("LoginByToken")]
+    public async Task LoginByToken() {
+
+    }
+
+}
