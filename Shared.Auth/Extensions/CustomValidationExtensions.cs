8ce03e25652fb8ceb47324bc7c89a758af444290
MasoudSh <Msh.Git.Dev@gmail.com>
2024-04-16 21:33:22 +0330

    create : SignUpValidaor and some shared extensions (using fluentValdator) and add it lib to csprojs

---
+++ Shared.Auth/Extensions/CustomValidationExtensions.cs
@@ -0,0 +1,52 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Shared.Auth.Models;
+
+namespace Shared.Auth.Extensions;
+
+public static class CustomValidationExtensions {
+
+    public static IRuleBuilderOptions<TModel , string> BeInRange<TModel>(
+      this IRuleBuilder<TModel , string> ruleBuilder,string propertyName , int min = 7 , int max = 80)
+      => ruleBuilder
+          .NotNullOrWhiteSpace(propertyName)
+          .MinimumLength(min).WithMessage($"The <{propertyName}> must have at least {min} characters.")
+          .MaximumLength(max).WithMessage($"The <{propertyName}> must have at most {max} characters.");
+
+
+    public static IRuleBuilderOptions<TModel , string> NotNullOrWhiteSpace<TModel>(
+        this IRuleBuilder<TModel , string> ruleBuilder , string propertyName)
+        => ruleBuilder
+         .Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage($"The <{propertyName}> can not be <NullOrWhiteSpace>.");
+
+    public static IRuleBuilderOptions<TModel , string> BeUserNameChars<TModel>(
+     this IRuleBuilder<TModel , string> ruleBuilder , string propertyName)
+     => ruleBuilder
+         .BeInRange(propertyName)
+         .Must(x => x.All(char.IsLetterOrDigit))
+         .WithMessage($"The <{propertyName}> can only contain letters and numbers.");
+
+    public static IRuleBuilderOptions<TModel , string> BePasswordChars<TModel>(
+    this IRuleBuilder<TModel , string> ruleBuilder , string propertyName)
+    => ruleBuilder
+        .BeInRange(propertyName)
+        .Must(x => x.Any(char.IsUpper))
+        .WithMessage($"The <{propertyName}> must contain at least one uppercase letter.")
+        .Must(x => x.Any(char.IsLower))
+        .WithMessage($"The <{propertyName}> must contain at least one lowercase letter.")
+        .Must(x => x.Any(char.IsDigit))
+        .WithMessage($"The <{propertyName}> must contain at least one digit.")
+        .Must(x => x.Any(char.IsPunctuation))
+        .WithMessage($"The <{propertyName}> must contain at least one punctuation mark.");
+
+
+    public static List<CodeMessage> AsCodeMessages(this List<ValidationFailure> validationFailures) {
+        var errors = new List<CodeMessage>();
+        Parallel.ForEach(validationFailures , item => {
+            errors.Add(new(item.PropertyName , item.ErrorMessage));
+        });
+        return errors;
+    }
+
+
+}
---
+++ Shared.Auth/ModelValidators/SignUpValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using Shared.Auth.DTOs;
+using Shared.Auth.Extensions;
+
+
+namespace Shared.Auth.ModelValidators;
+
+public class SignUpValidator : AbstractValidator<SignUpDto> {
+
+    public SignUpValidator() {
+        CheckEmail();
+        CheckUserName();
+        CheckPasswords();
+
+    }
+
+    private void CheckPasswords() {
+        string propertyName = "Password";
+        RuleFor(x => x.Password).BePasswordChars(propertyName);
+        RuleFor(x => x.ConfirmedPassword).Equal(x => x.Password)
+            .WithMessage("The <Password> do not match with <ConfirmPassword>.");
+    }
+
+    private void CheckUserName() {
+        string propertyName = "UserName";
+        RuleFor(x => x.UserName).BeUserNameChars(propertyName);
+    }
+
+
+    private void CheckEmail() =>
+        RuleFor(x => x.Email)
+        .EmailAddress()
+        .WithMessage("Please enter a valid email address.");
+
+
+
+}
