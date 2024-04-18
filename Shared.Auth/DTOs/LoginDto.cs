f28d037fe0a563a7dbdeebbb478b0666a596572b
MasoudSh <Msh.Git.Dev@gmail.com>
2024-04-18 14:54:28 +0330

    update : remove LoginType and change GetValue

--- Shared.Auth/DTOs/LoginDto.cs
+++ Shared.Auth/DTOs/LoginDto.cs
@@ -1,27 +1,22 @@
-﻿using Shared.Auth.Enums;
-
-namespace Shared.Auth.DTOs;
+﻿namespace Shared.Auth.DTOs;
 
 public record LoginDto {
 
-
-    public LoginType LoginType { get; set; } = LoginType.UserName;
+    /// <summary>
+    /// LoginName Can be Email or UserName.
+    /// </summary>
     public string LoginName { get; set; }
     public string Password { get; set; }
     public bool IsPersistent { get; set; } = false;
 
-    //for search faster
-    public DateTime? MembershipDate { get; set; }
-    public DateTime? BirthDate { get; set; }
-    public Gender? Gender { get; set; }
-
     // for security
-    public string? Captcha { get; set; }
+    public string Captcha { get; set; } = String.Empty;
 
-    public (
-        LoginType LoginType ,
-        string LoginName ,
-        string Password ,
-        bool IsPersistent) GetValues()
-        => (LoginType, LoginName, Password, IsPersistent);
+    //Enable these later for search faster
+    //public DateTime? MembershipDate { get; set; }
+    //public DateTime? BirthDate { get; set; }
+    //public Gender? Gender { get; set; }
+
+    public (string LoginName, string Password, bool IsPersistent , string Captcha) GetValues()
+        => (LoginName, Password, IsPersistent, Captcha);
 }
