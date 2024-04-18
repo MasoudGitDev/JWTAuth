6e8d52ca6b1a0320ab44c05ba8459276960381d3
MasoudSh <Msh.Git.Dev@gmail.com>
2024-04-18 14:47:57 +0330

    create : RegexType (for Email)

---
+++ Shared.Auth/RegularExpressions/RegexType.cs
@@ -0,0 +1,6 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Shared.Auth.RegularExpressions;
+public static class RegexType {
+    public static Regex Email => new(@"^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$");
+}
