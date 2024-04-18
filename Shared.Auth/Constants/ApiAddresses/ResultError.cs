a7adc358c79a314d016c6462b2a09b5f16b7d998
MasoudSh <Msh.Git.Dev@gmail.com>
2024-04-18 14:47:03 +0330

    create : ResultError (to have some CodeMessage values)

---
+++ Shared.Auth/Constants/ApiAddresses/ResultError.cs
@@ -0,0 +1,10 @@
+﻿using Shared.Auth.Models;
+
+namespace Shared.Auth.Constants.ApiAddresses;
+public record ResultError(string Code , string Message) {
+
+    public static ResultError InvalidToken => new("InvalidToken" , "The <token> is invalid.");
+
+    public static implicit operator CodeMessage(ResultError error) => new(error.Code, error.Message);
+
+}
