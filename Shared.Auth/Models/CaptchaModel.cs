037ab414f06ac23385e48f4cdf27fe9d52324955
MasoudSh <Msh.Git.Dev@gmail.com>
2024-04-19 22:58:14 +0330

    create : CaptchaModel

---
+++ Shared.Auth/Models/CaptchaModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Shared.Auth.Models;
+public class CaptchaModel {
+    public CaptchaStatus Status { get; set; } = CaptchaStatus.None;
+    public string FileName { get; set; } = String.Empty;
+    /// <summary>
+    /// The Value of 'Image' must be in base64string!
+    /// </summary>
+    public string Image { get; set; } = String.Empty;
+    public string UserInput { get; set; } = String.Empty;
+
+    public static CaptchaModel Empty => new();
+}
+
+public enum CaptchaStatus {
+    None = 0 , Failed = 1 , Succeed = 2
+}
