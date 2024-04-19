ef90f74e2202f81e6d114acf4d0db57c3a4765e7
MasoudSh <Msh.Git.Dev@gmail.com>
2024-04-19 22:59:59 +0330

    create : CaptcahAction toget sever api address

---
+++ Shared.Auth/Constants/ApiAddresses/CaptchaAction.cs
@@ -0,0 +1,14 @@
+﻿namespace Shared.Auth.Constants.ApiAddresses;
+
+public record CaptchaAction(string Name) {
+
+    private static string BaseAddress(string name) => "Api/Captcha/" + name;
+
+    public static AccountAction Generate => new(BaseAddress("Generate"));
+    public static AccountAction Validate => new(BaseAddress("Validate"));
+
+    public override string ToString() {
+        return Name;
+    }
+    public static implicit operator string(CaptchaAction accountAction) => accountAction.Name;
+}
