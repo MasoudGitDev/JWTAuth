211fe0024d3a7bb1d1b4342527b7105c7a342990
MasoudSh <Msh.Git.Dev@gmail.com>
2024-04-19 22:59:28 +0330

    create : CaptchaManagerService , Captcha.razor ,css

---
+++ Presentations/ClientApp/Services/ICaptchaManagerService.cs
@@ -0,0 +1,12 @@
+﻿namespace ClientApp.Services;
+
+public interface ICaptchaManagerService {
+    Task<(string fileName, string base64Str)> GenerateAsync();
+}
+
+public class CaptchaManagerService(HttpClient _httpClient) 
+    : HttpClientManager(_httpClient) ,  ICaptchaManagerService {
+    public async Task<(string fileName, string base64Str)> GenerateAsync() {
+        return await GetCaptchaAsync();
+    }
+}
