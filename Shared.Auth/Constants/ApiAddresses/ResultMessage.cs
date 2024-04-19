526c776d9ae499c3befbc2ebc1e89d7dca7493e9
MasoudSh <Msh.Git.Dev@gmail.com>
2024-04-19 23:06:55 +0330

    create : ResultMessage

---
+++ Shared.Auth/Constants/ApiAddresses/ResultMessage.cs
@@ -0,0 +1,11 @@
+﻿using Shared.Auth.Models;
+
+namespace Shared.Auth.Constants.ApiAddresses;
+
+public record ResultMessage(string Code , string Message) {
+
+    public static ResultMessage ValidCaptcha => new("ValidCaptcha" , "The <captcha> is validated successfully.");
+
+    public static implicit operator CodeMessage(ResultMessage model) => new(model.Code , model.Message);
+
+}
