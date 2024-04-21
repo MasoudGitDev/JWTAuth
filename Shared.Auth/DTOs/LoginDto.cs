2320da85a09b3a5cb5285b11cff78d3f28fa82ed
MasoudSh <Msh.Git.Dev@gmail.com>
2024-04-21 09:57:31 +0330

    refactor : remove captcha prop

--- Shared.Auth/DTOs/LoginDto.cs
+++ Shared.Auth/DTOs/LoginDto.cs
@@ -7,16 +7,13 @@ public record LoginDto {
     /// </summary>
     public string LoginName { get; set; }
     public string Password { get; set; }
-    public bool IsPersistent { get; set; } = false;
-
-    // for security
-    public string Captcha { get; set; } = String.Empty;
+    public bool IsPersistent { get; set; } = false;    
 
     //Enable these later for search faster
     //public DateTime? MembershipDate { get; set; }
     //public DateTime? BirthDate { get; set; }
     //public Gender? Gender { get; set; }
 
-    public (string LoginName, string Password, bool IsPersistent , string Captcha) GetValues()
-        => (LoginName, Password, IsPersistent, Captcha);
+    public (string LoginName, string Password, bool IsPersistent) GetValues()
+        => (LoginName, Password, IsPersistent);
 }
