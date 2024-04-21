b1b0a8a5ea1a4346455491b8acb4c609f8108149
MasoudSh <Msh.Git.Dev@gmail.com>
2024-04-21 09:44:49 +0330

    update : using cache instead of session [beacuse blazor not work]

--- Presentations/AuthWebAPI/Controllers/Utilities/CaptchaController.cs
+++ Presentations/AuthWebAPI/Controllers/Utilities/CaptchaController.cs
@@ -1,32 +1,37 @@
 ﻿using Apps.Services.Services;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
 using Shared.Auth.Constants.ApiAddresses;
+using Shared.Auth.DTOs;
 using Shared.Auth.Exceptions;
 using Shared.Auth.Models;
 
 namespace AuthWebAPI.Controllers.Utilities;
 [Route("Api/[controller]")]
 [ApiController]
-public class CaptchaController(ICaptcha _captcha) : ControllerBase {
+public class CaptchaController(ICaptcha _captcha , IDistributedCache _cache) : ControllerBase {
 
     [HttpGet("Generate")]
     public async Task<IActionResult> GenerateCaptcha() {
         var fileName = "captcha_" + Guid.NewGuid().ToString().Replace("-", "") + ".png";
-        var result = await _captcha.GenerateAsync();
-        HttpContext.Session.SetString(fileName , result.ImageText ?? "<invalid-image>");
-        return new FileContentResult(result.Image , "image/png") { FileDownloadName = fileName };
+        var (Image, ImageText) = await _captcha.GenerateAsync();
+        // HttpContext.Session.SetString(fileName , result.ImageText ?? "<invalid-image>");
+        await _cache.SetStringAsync(fileName , ImageText);
+        return new FileContentResult(Image , "image/png") { FileDownloadName = fileName };
     }
 
-    
+
     [HttpPost("Validate")]
-    public CodeMessage ValidateCaptcha(string fileName , string userInput) {
-        var captchaValue =
-            HttpContext.Session.GetString(fileName)
-            ?? throw new AccountException("InvalidCaptcha" , $"This <captcha> key : <{fileName}> is invalid.");
+    public async Task<CodeMessage> ValidateCaptcha(CaptchaValidationDto model) {
+        var (fileName, userInput) = (model.FileName, model.UserInput);
+        var captchaValue =await _cache.GetStringAsync(fileName);
+        if(String.IsNullOrWhiteSpace(captchaValue)) {
+            throw new AccountException("InvalidCaptcha" , $"This <captcha> key : <{fileName}> is invalid.");
+        }
         if(!userInput.Equals(captchaValue , StringComparison.InvariantCultureIgnoreCase)) {
             throw new AccountException("InvalidCaptcha" , $"This <captcha> value : <{userInput}> is invalid.");
         }
+        await _cache.RemoveAsync(fileName);
         return ResultMessage.ValidCaptcha;
     }
 }
