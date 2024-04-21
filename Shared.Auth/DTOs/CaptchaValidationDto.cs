317c6c5f25d6b2d2591565bbf1c8675080ed98f8
MasoudSh <Msh.Git.Dev@gmail.com>
2024-04-21 09:40:32 +0330

    create : CaptchaValidation DTO

---
+++ Shared.Auth/DTOs/CaptchaValidationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Auth.DTOs;
+public class CaptchaValidationDto {
+
+    public static CaptchaValidationDto New(string fileName , string userInput)
+        => new(){ UserInput = userInput , FileName = fileName };
+
+    public  string FileName { get; set; }
+    public  string UserInput { get; set; } 
+}
