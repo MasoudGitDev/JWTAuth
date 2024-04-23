c6c82aa5a471a111b70c2df2a0d9e6b4163a5059
MasoudSh <Msh.Git.Dev@gmail.com>
2024-04-23 11:56:26 +0330

    create : Login Validator

---
+++ Shared.Auth/ModelValidators/LoginValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Shared.Auth.DTOs;
+using Shared.Auth.Extensions;
+
+namespace Shared.Auth.ModelValidators;
+public class LoginValidator : AbstractValidator<LoginDto> {
+
+    public LoginValidator()
+    {
+        CheckLoginName();
+        CheckPasswords();
+    }
+
+    private void CheckLoginName() {
+        RuleFor(x=>x.LoginName).Must(x=> !String.IsNullOrWhiteSpace(x))
+            .WithMessage($"The <login-name> can not be null or whiteSpace.");
+    }
+
+    private void CheckPasswords() {
+        string propertyName = "Password";
+        RuleFor(x => x.Password).BePasswordChars(propertyName);
+    }
+
+
+}
