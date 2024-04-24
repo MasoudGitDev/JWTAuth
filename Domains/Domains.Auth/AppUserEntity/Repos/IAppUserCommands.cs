0d6c896bb3938cfc406edf07754a09d3a0200035
MasoudSh <Msh.Git.Dev@gmail.com>
2024-04-24 11:04:17 +0330

    refactor : remove comment

--- Domains/Domains.Auth/AppUserEntity/Repos/IAppUserCommands.cs
+++ Domains/Domains.Auth/AppUserEntity/Repos/IAppUserCommands.cs
@@ -3,16 +3,10 @@ using Shared.Auth.Models;
 
 namespace Domains.Auth.AppUserEntity.Repos;
 
-/// <summary>
-/// ActionLink : (userId,token) => result
-/// </summary>
 public interface IAppUserCommands {
-
     Task<AccountResult> SignUpAsync(AppUser appUser , string password , LinkModel model);
-
     Task<AccountResult> SignInAsync(AppUser appUser , string password , bool isPersistent , bool lockoutOnFailure);
     Task<AccountResult> LoginByTokenAsync(string authToken);
-
 }
 
 
