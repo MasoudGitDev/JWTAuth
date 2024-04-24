f742f6c4b9f0f5a88e9b1f6ce352c69c128081c1
MasoudSh <Msh.Git.Dev@gmail.com>
2024-04-24 11:25:46 +0330

    create : Assembly file

---
+++ Infrastructures/Infra.Auth/Assembly.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("Tests.Infra.Auth")]
+
+namespace Infra.Auth;
+internal class Assembly {
+    internal static System.Reflection.Assembly GetExecutingAssembly() {
+        throw new NotImplementedException();
+    }
+}
