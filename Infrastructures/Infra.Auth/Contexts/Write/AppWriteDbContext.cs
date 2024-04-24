f8414858083a018fdeb3d9d8c3d9fae6637853e4
MasoudSh <Msh.Git.Dev@gmail.com>
2024-04-24 11:25:16 +0330

    refactor: remove comments

--- Infrastructures/Infra.Auth/Contexts/Write/AppWriteDbContext.cs
+++ Infrastructures/Infra.Auth/Contexts/Write/AppWriteDbContext.cs
@@ -1,9 +1,7 @@
 ﻿using Domains.Auth.AppRoleEntity;
 using Domains.Auth.AppUserEntity.Aggregate;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System.Reflection;
 
 namespace Infra.Auth.Contexts.Write;
 internal class AppWriteDbContext : IdentityDbContext<AppUser , AppRole , Guid> {
@@ -12,18 +10,9 @@ internal class AppWriteDbContext : IdentityDbContext<AppUser , AppRole , Guid> {
     }
 
     protected override void OnModelCreating(ModelBuilder builder) {
-
         builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly() , ContextType);
         base.OnModelCreating(builder);
-
-
-
     }
-
-    //public DbSet<MaleAppUser> MaleAppUsers { get; set; }
-    //public DbSet<FemaleAppUser> FemaleAppUsers { get; set; }
-
-
     private static bool ContextType(Type type)
         => type.FullName?.Contains("Configs.Write") ?? false;
 }
