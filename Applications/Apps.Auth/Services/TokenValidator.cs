2afb5712b567c630bd026cb951bb4cc69eb77a69
MasoudSh <Msh.Git.Dev@gmail.com>
2024-04-24 10:07:16 +0330

    refactor : change names

--- Applications/Apps.Auth/Services/TokenValidator.cs
+++ Applications/Apps.Auth/Services/TokenValidator.cs
@@ -4,16 +4,16 @@ namespace Apps.Auth.Services;
 
 internal class TokenValidator() {
 
-    private static SortedList<byte , TokenValidationChain> _validators => new() {
+    private static SortedList<byte , TokenValidationChain> Validators => new() {
          // { 0 , new ItemCountsValidation(7) } ,
-      //  { 1 , new IsBlockedValidation() },      
+         //  { 1 , new IsBlockedValidation() },      
         { 2 , new DateTimeValidation(60) },
         { 3 , new IssuerValidation("https://localhost:7224") },
         { 4 , new AudiencesValidation(["https://localhost:7224"]) }
     };
 
     public static bool Validate(Dictionary<string , string> _claims) {
-        foreach(var validator in _validators) {
+        foreach(var validator in Validators) {
             validator.Value.Apply(_claims);
         }
         return true;
