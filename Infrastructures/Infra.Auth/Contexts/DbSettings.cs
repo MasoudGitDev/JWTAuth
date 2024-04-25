7cffcec50c7a819eb42f9669f575c7648221ccaf
MasoudSh <Msh.Git.Dev@gmail.com>
2024-04-25 11:31:17 +0330

    refactor : change db path

--- Infrastructures/Infra.Auth/Contexts/DbSettings.cs
+++ Infrastructures/Infra.Auth/Contexts/DbSettings.cs
@@ -2,6 +2,6 @@
 public static class DbSettings {
 
     public static readonly string connectionString =
-    @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AuthDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=JwtAppDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
 }
